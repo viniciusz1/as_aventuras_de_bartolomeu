e73544db8a9d4431a106103b46155def4ac29d69
Your Name <thiagomarinsbraga@gmail.com>
2022-07-27 21:51:18 -0300

    21:50 - 27/07
    Cenário 2 feito e mias alguns componentes criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { Cenario1Component } from './cenario1/cenario1.component';
 import { MenuComponent } from './menu/menu.component';
 import { Cenario2Component } from './cenario2/cenario2.component';
+import { Parte1Component } from './parte1/parte1.component';
+import { Parte2Component } from './parte2/parte2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Cenario1Component,
     MenuComponent,
-    Cenario2Component
+    Cenario2Component,
+    Parte1Component,
+    Parte2Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/parte2/parte2.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Parte2Component } from './parte2.component';
+
+describe('Parte2Component', () => {
+  let component: Parte2Component;
+  let fixture: ComponentFixture<Parte2Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Parte2Component ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Parte2Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
