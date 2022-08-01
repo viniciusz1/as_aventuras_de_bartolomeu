50d908f4320c1d80369cebea48396387a6e5242f
Thiago-M-Braga <106108469+Thiago-M-Braga@users.noreply.github.com>
2022-08-01 13:26:34 -0300

    13:26 - 01/08
    Removendo teste de vida do dragão do menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -9,15 +9,8 @@ export class MenuComponent implements OnInit {
 
   constructor() { }
 
-  click = 0;
-  vida = 120;
 
   ngOnInit(): void {
-  }
-
-  aumentarClick() {
-    this.click++;
-    this.vida = this.vida-1
   }
 
   
