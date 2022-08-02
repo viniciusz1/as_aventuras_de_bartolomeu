c13b189f814ee607d358a16e9d553acd85dd1031
Thiago-M-Braga <thiagomarinsbraga@gmail.com>
2022-08-02 18:49:00 -0300

    18:48 - 02/08
    Cenario criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { AppComponent } from './app.component';
 import { Cenario1Component } from './cenario1/cenario1.component';
 import { MenuComponent } from './menu/menu.component';
 import { Cenario2Component } from './cenario2/cenario2.component';
+import { Cenario3Component } from './cenario3/cenario3.component';
 import { Parte1Component } from './parte1/parte1.component';
 import { Parte2Component } from './parte2/parte2.component';
+import { Parte3Component } from './parte3/parte3.component';
 import { RotasModule } from './rotas.module';
 import { FormsModule } from '@angular/forms';
 import { AppService } from './app.service';
@@ -17,8 +19,10 @@ import { AppService } from './app.service';
     Cenario1Component,
     MenuComponent,
     Cenario2Component,
+    Cenario3Component,
     Parte1Component,
     Parte2Component,
+    Parte3Component,
     
   ],
   imports: [
--- src/app/rotas.module.ts
+++ src/app/rotas.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { Parte3Component } from './parte3/parte3.component';
 import { Parte2Component } from './parte2/parte2.component';
 import { Parte1Component } from './parte1/parte1.component';
 
@@ -15,6 +16,10 @@ import { Parte1Component } from './parte1/parte1.component';
       {
         path:'',
         component: MenuComponent
+      },
+      {
+        path:'nivel-3',
+        component: Parte3Component
       },
       {
         path:'nivel-2',
